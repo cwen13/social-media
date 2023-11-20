b21100b9a1488667c0c7c4c097a41a1e645eeca7
Cody Wenrich <cwenrich13@gmail.com>
2023-11-19 22:45:38 -0500

    update CSP spelling

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use((req,res, nenxt) => {
+app.use((req, res, next) => {
   res.setHeader(
     "Content-Security-Policy",
     "default-src 'self'; script-src 'self' 'unsafe-inline';"
