8a2a4c1319f1d668286fa00e9061780f457a1992
Cody Wenrich <cwenrich13@gmail.com>
2023-11-19 21:45:20 -0500

    IN PROCESS: getting env working

--- server/server.js
+++ server/server.js
@@ -1,15 +1,14 @@
+require("dotenv").config({ path: "./.env" });
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 const { authMiddleware } = require('./utils/auth');
 
-const { typeDefs, resolvers } = require('./schemas/index');
+const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 
-require("dotenv").config();
 
 const PORT = process.env.PORT || 3001;
-console.log(process.env);
 const server = new ApolloServer({
   typeDefs,
   resolvers,
