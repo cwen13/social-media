3e9051bdff683b98d9645db96a606ce9ea36894d
Cody Wenrich <cwenrich13@gmail.com>
2023-11-19 22:21:41 -0500

    update build

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,8 @@
 const Sequelize = require('sequelize');
 require("dotenv").config();
 
+console.log(process.env);
+
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
       : new Sequelize(process.env.DB_NAME,
