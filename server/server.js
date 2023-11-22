39f30c5e80ee937a485141d8b429d3eca7abacb2
Cody Wenrich <cwenrich13@gmail.com>
2023-11-21 22:41:39 -0500

    update for deployment

--- server/server.js
+++ server/server.js
@@ -17,14 +17,6 @@ const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use((req, res, next) => {
-  res.setHeader(
-    "Content-Security-Policy",
-    "default-src 'self'; script-src 'self' 'unsafe-inline';"
-    // Add more directives as needed
-  );
-  next();
-});
 
 // Serve up static assets
 if (process.env.NODE_ENV === 'production') {
